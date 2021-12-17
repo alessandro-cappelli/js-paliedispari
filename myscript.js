41ddbb5b824959dcd6dbd1ca8859280f7cfd9727
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-17 16:28:49 +0100

    parola divisa

--- myscript.js
+++ myscript.js
@@ -1,5 +1,16 @@
 // chiedere all`utente di inserire una parola 
 let parola = prompt('inserisci una parola')
-let parolaStringa = parola.length
-console.log(parolaStringa)
+
+// inserire quello che viene scritto nel prompt in una array
+let parolaArr = []
+// dividere la parola
+let parolaDivisa = parola.split('')
+parolaArr.push(parolaDivisa)
+
+console.log(parolaDivisa);
+console.log(parolaArr)
+
 // creare una funzione per capire se la parola inserita è palindroma
+// function funzionePalin(parola){
+//     if(parola = )
+// }
