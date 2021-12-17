ec1a3845121cfc45255e480b5ad238f309f97f0e
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-17 18:08:28 +0100

    if else

--- myscript.js
+++ myscript.js
@@ -9,19 +9,25 @@ let parolaDivisa = parola.split('');
 
 // push della parola divisa nell`array
 parolaArr.push(parolaDivisa);
-
-// console.log(parolaDivisa);
 console.log(parolaArr);
 
 
 // creare un array per la parola invertita
-let parolaInvertitaArr = []
+let parolaInvertitaArr = [];
 
 // invertire la parola inserita 
-let parolaInvertita = parolaDivisa.reverse('')
+let parolaInvertita = parolaDivisa.reverse('');
 
 // push della parola invertita nell`array 
-parolaInvertitaArr.push(parolaInvertita)
-console.log(parolaInvertitaArr)
+parolaInvertitaArr.push(parolaInvertita);
+console.log(parolaInvertitaArr);
 
-// creare una funzione per capire se la parola inserita è palindroma
+
+
+if(parolaArr=parolaInvertitaArr){
+    console.log('PAROLA PALINDROMA');
+}else{
+    console.log('PAROLA NON PALINDROMA');
+}
+    
+
