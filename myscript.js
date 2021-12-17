a6919698fa40f0832c718a0c5da47aef00fa2da5
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-17 15:41:42 +0100

    parolaStringa

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 // chiedere all`utente di inserire una parola 
-let parola = prompt('scrivi una parola')
-
+let parola = prompt('inserisci una parola')
+let parolaStringa = parola.length
+console.log(parolaStringa)
 // creare una funzione per capire se la parola inserita è palindroma
