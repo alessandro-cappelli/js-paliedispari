e7fb192b173c1cd7a082898dc51c03708a4456a7
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-17 16:45:41 +0100

    push array parolaInvertita

--- myscript.js
+++ myscript.js
@@ -1,16 +1,27 @@
 // chiedere all`utente di inserire una parola 
-let parola = prompt('inserisci una parola')
+let parola = prompt('inserisci una parola');
 
-// inserire quello che viene scritto nel prompt in una array
-let parolaArr = []
+// crare una array
+let parolaArr = [];
+
 // dividere la parola
-let parolaDivisa = parola.split('')
-parolaArr.push(parolaDivisa)
+let parolaDivisa = parola.split('');
 
-console.log(parolaDivisa);
-console.log(parolaArr)
+// push della parola divisa nell`array
+parolaArr.push(parolaDivisa);
+
+// console.log(parolaDivisa);
+console.log(parolaArr);
+
+
+// creare un array per la parola invertita
+let parolaInvertitaArr = []
+
+// invertire la parola inserita 
+let parolaInvertita = parolaDivisa.reverse('')
+
+// push della parola invertita nell`array 
+parolaInvertitaArr.push(parolaInvertita)
+console.log(parolaInvertitaArr)
 
 // creare una funzione per capire se la parola inserita è palindroma
-// function funzionePalin(parola){
-//     if(parola = )
-// }
